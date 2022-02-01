45a05f33dfafd21f7333d60d32c124acbcf38fc0
EC2 Default User <ec2-user@ip-172-31-4-17.us-east-2.compute.internal>
2022-02-01 13:40:32 +0000

    refactored book_view by extracting common erb file code to a layout file and by adding a dynamic chapter route, before filter, and view helper method to the rb file

--- assignments/book_view/book_viewer.rb
+++ assignments/book_view/book_viewer.rb
@@ -2,17 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @table_of_contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(chpter_content)
+    chpter_content.split("\n\n")
+  end
+end
+
 get "/" do  # assign a route to the "/" URL
   #File.read "public/template.html"  # Sinatra executes the body of this block & the return value is sent to the user's browser
   @title = "The Adventures of Sherlock Holmes"
-  @table_of_contents = File.readlines("data/toc.txt")
+
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @table_of_contents = File.readlines("data/toc.txt")
-  @chapter_1 = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  chp_number = params[:number].to_i
+  chp_name = @table_of_contents[chp_number - 1]
+  @title = "Chapter #{chp_number}: #{chp_name}"
+
+  @chapter_contents = File.read("data/chp#{chp_number}.txt")
 
   erb :chapter
 end
