e63638c8f7249ba1894ab8aea83a6c58d2ef70fd
EC2 Default User <ec2-user@ip-172-31-4-17.us-east-2.compute.internal>
2022-02-12 21:40:26 +0000

    added page to view individual list and started individual list edit functionality

--- assignments/todos/todo.rb
+++ assignments/todos/todo.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -32,6 +33,40 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# view individual list
+get "/lists/:id" do
+  @list_index = params[:id].to_i
+  @lists = session[:lists]
+  @selected_list = @lists[@list_index]
+
+  erb :individual_list, layout: :layout
+end
+
+# Retrieve page to edit existing list
+get "/lists/:id/edit" do
+  list_index = params[:id].to_i
+  @list = session[:lists][list_index]
+  erb :edit_list, layout: :layout
+end
+
+# allow user to revise an existing list
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  index = params[:id].to_i
+  @list = session[:lists][index]
+  
+  error = list_name_as_error(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list name has been revised."
+    redirect "/lists/#{index}>"
+  end
+end
+
+
 # Return nil if list name is valid; otherwise return error message
 def list_name_as_error(name)
   if !(2..100).cover? name.size
@@ -46,7 +81,8 @@ end
 post "/lists" do
   list_name = params[:list_name].strip
 
-  if error = list_name_as_error(list_name)
+  error = list_name_as_error(list_name)
+  if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
