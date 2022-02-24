83532242590dacf38c5e0e0dfcb9cd58e843b69c
EC2 Default User <ec2-user@ip-172-31-4-17.us-east-2.compute.internal>
2022-02-24 02:07:49 +0000

    added redirect for nonexistent urls and markdown rendering for md files along with tests

--- assignments/filebased_cms/cms_main.rb
+++ assignments/filebased_cms/cms_main.rb
@@ -2,6 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
+require "redcarpet"
+
+configure do
+  enable :sessions
+  set :session_secret, 'notanactualsecret'
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def file_content(location)
+  content = File.read(location)
+  case File.extname(location)
+  when ".md"
+    render_markdown(content)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  end
+end
 
 get "/" do
   @scrubbed_list_of_files = Dir.entries("data").select do |filename|
@@ -11,6 +33,10 @@ get "/" do
 end
 
 get "/:filename" do
-  @contents = File.readlines("data/#{params[:filename]}")
-  erb :file_contents
+  if File.exist?("data/#{params[:filename]}")
+    file_content("data/#{params[:filename]}")
+  else
+    session[:error] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end
